9335c482f6f4cfda4654e5647adebc5d7fb10fc2
Ben Fitzhardinge <ben.fitzhardinge@ausvet.com.au>
2018-10-05 12:19:00 +0800

    Updated for version 0.1.0

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from distutils.core import setup
 
 setup(
     name='apistar-dynamic',
-    version='1.0.0',
-    description='Omnisyan Database Class shared resource',
+    version='0.1.0',
+    description='API Star Dynamic model and handler generator.',
     author='Ben Fitzhardinge',
     author_email='ben@fitzhardinge.net',
     url='https://github.com/bendog/apistar-dynamic',
